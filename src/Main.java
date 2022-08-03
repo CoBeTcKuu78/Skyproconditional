5d0e2ff818a957e312e8b2e035107df29bc11649
CoBeTcKuu <9437855@gmail.com>
2022-08-03 21:31:38 +0300

    Второй коммит

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 19;
+        int age = 20;
         if (age >= 18) {
             System.out.printf("Ты можешь водить");
         } else {
