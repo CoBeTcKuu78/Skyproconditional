fe42cf09b754e5101f9099a631d553efebef8e1f
CoBeTcKuu <9437855@gmail.com>
2022-08-03 21:44:23 +0300

    Второй пример

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 20;
-        if (age >= 18) {
-            System.out.printf("Ты можешь водить");
-        } else {
-            System.out.printf("Ты не можешь водить");
+        int age = 19;
+        if (age >= 18 && age < 21) {
+            System.out.printf("ты достаточно взрослый чтобы водить, но не достаточно взрослый чтобы пить алкоголь");
         }
     }
 }
