223180aa46829b057699e39a01ec3b37f89becaf
CoBeTcKuu <9437855@gmail.com>
2022-08-14 20:44:53 +0300

    коммит примеры условных операторов (пример 4) повторение

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,38 @@
-// пример 4
 public class Main {
     public static void main(String[] args) {
-    int age = 12;
-    if (age == 18){
-        System.out.println("С окончанием школы!");
-    } else if (age == 21) {
-        System.out.println("Поздравляю с совершеннолетием!");
-    } else if (age == 7){
-        System.out.println("Иди в школу!");
-    } else {
-        System.out.println("Ничего не произошло...");
-    }
+        /* int age = 18;
+        if (age >= 18) {
+            System.out.println("Иди праздновать!");
+            if (age < 21) {
+                System.out.println("Выпей словочного пива!");
+            } else {
+                System.out.println("Можешь пить алкоголь!");
+            }
+        } else if (age > 7) {
+                System.out.println("Иди в школу!");
+            } else {
+                System.out.println("Иди в детский сад!");
+            }
+        /* boolean canNotGoToScool = age < 7 || age > 18;
+        if (age >= 18 && age < 21){
+             System.out.println("Ты достаточно взрослый чтобы водить, но не достаточно взрослый, чтобы пить алкоголь!");
+        }
+        if (canNotGoToScool) {
+           System.out.println("Я думаю, ты не ходишь в школу!");
+        }
+           System.out.println();
+        boolean skyIsClear = true;
+        if (skyIsClear) {
+            System.out.println("Небо чистое!");
+        } */
+        System.out.println();
+        int age = 6;
+        if (age == 18 ) {
+            System.out.println("С окончанием школы!");
+        } else if (age == 21){
+            System.out.println("Теперь можно пить алкоголь!");
+        } else {
+            System.out.println("Ничего не происходит!");
+        }
     }
 }
